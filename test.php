a63874d080d6c02d7d2c9f826cef357d606c4daf
Ihor Bilous <i.bilous@astoundcommerce.com>
2024-12-18 13:48:06 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+// This is a dummy PHP script
+
+echo "Hello, World!";
+
+// Dummy function
+function dummyFunction() {
+    return "This is a dummy function.";
+}
+
+// Call the dummy function
+echo dummyFunction();
+?>
